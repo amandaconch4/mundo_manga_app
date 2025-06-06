a8cc0cb0ca2a028291bf6b021482c6818774394d
Amanda <aman.concha@duocuc.cl>
2025-06-06 19:34:02 -0400

    se instala angular material y se añade el forms de registro

--- src/app/pages/registro/registro.module.ts
+++ src/app/pages/registro/registro.module.ts
@@ -1,19 +1,26 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-
 import { IonicModule } from '@ionic/angular';
-
 import { RegistroPageRoutingModule } from './registro-routing.module';
-
 import { RegistroPage } from './registro.page';
+
+// Imports para el calendario
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatNativeDateModule } from '@angular/material/core';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input'; 
 
 @NgModule({
   imports: [
     CommonModule,
     FormsModule,
     IonicModule,
-    RegistroPageRoutingModule
+    RegistroPageRoutingModule,
+    MatDatepickerModule,
+    MatNativeDateModule,
+    MatFormFieldModule,
+    MatInputModule
   ],
   declarations: [RegistroPage]
 })
--- src/app/pages/registro/registro.page.ts
+++ src/app/pages/registro/registro.page.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component} from '@angular/core';
+import { AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'app-registro',
@@ -6,11 +7,49 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./registro.page.scss'],
   standalone: false,
 })
-export class RegistroPage implements OnInit {
 
-  constructor() { }
+export class RegistroPage {
+  user: string = '';
+  nombre: string = '';
+  correo: string = '';
+  password: string = '';
+  nivelEducacional: string = '';
+  fechaNacimiento: string = '';
 
-  ngOnInit() {
+  constructor(private alertController: AlertController) { }
+
+  limpiarFormulario() {
+
+    this.user = '';
+    this.nombre = '';
+    this.correo = '';
+    this.password = '';
+    this.nivelEducacional = '';
+    this.fechaNacimiento = '';
   }
 
+  async registrar() {
+    if (!this.user || !this.nombre || !this.correo || !this.password || !this.nivelEducacional || !this.fechaNacimiento) {
+      const alert = await this.alertController.create({
+        header: 'Error',
+        message: 'Por favor, completa todos los campos.',
+        buttons: ['OK']
+      });
+      await alert.present();
+      return;
+    }
+
+    //Si pasan todos los campos, se muestra mensaje de éxito y muestra los datos ingresados
+    const alert = await this.alertController.create({
+      header: 'Registro exitoso, estos son sus datos',
+      message: `
+        Nombre completo: ${this.nombre}
+        Nivel Educacional: ${this.nivelEducacional}
+        Fecha de Nacimiento: ${this.fechaNacimiento}
+      `,
+      buttons: ['OK']
+    });
+
+    await alert.present();
+  }
 }
